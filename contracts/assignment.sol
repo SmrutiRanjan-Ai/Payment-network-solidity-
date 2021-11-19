c416adbe8ee45ba4999e87eccf005e3d5f835f9d
Smruti Ranjan Behera <85878075+SmrutiRanjan-Ai@users.noreply.github.com>
2021-11-19 18:41:36 +0530

    solidity updated

--- contracts/assignment.sol
+++ contracts/assignment.sol
@@ -29,7 +29,7 @@ contract Storage {
  constructor(){
     
  }
-function registerUser(uint _user_id, string calldata _user_name) public {
+function registerUser(uint _user_id, string memory _user_name) public {
       
      User storage u = users[_user_id];
      u.userid=_user_id;
@@ -37,12 +37,13 @@ function registerUser(uint _user_id, string calldata _user_name) public {
  }
 
 
-function createAcc(uint _user_id_1, uint _user_id_2) public {
-    JointAccList[acc_num]= Joint(_user_id_1,_user_id_2,0,0);
+function createAcc(uint _user_id_1, uint _user_id_2,uint bal1,uint bal2) public returns (uint account_){
+    JointAccList[acc_num]= Joint(_user_id_1,_user_id_2,bal1,bal2);
     users[_user_id_1].JointAcc[_user_id_2]= acc_num;
     users[_user_id_2].JointAcc[_user_id_1]= acc_num;
     adjmatrix[_user_id_1].push(_user_id_2);
     adjmatrix[_user_id_2].push(_user_id_1);
+    account_=acc_num;
     acc_num+=1;
     
     
@@ -57,6 +58,29 @@ function sender(uint _user_id_1,uint _user_id_2,uint amount) internal{
     
     
 }
+function verifybalance(uint _user_id_1,uint _user_id_2,uint amount) internal view returns (bool status){
+    
+    uint num=users[_user_id_1].JointAcc[_user_id_2];
+    if(JointAccList[num].user_id_1_bal>=amount){
+        status = true;
+    }
+    else{
+        status = false;
+    }
+    
+    
+    
+}
+
+function checkbalancebyaccnum(uint num) public view returns (uint balance_)
+{
+    balance_ = JointAccList[num].user_id_1_bal+JointAccList[num].user_id_2_bal;
+}
+function checkbalancebyuserid(uint _user_id_1,uint _user_id_2) public view returns (uint balance_)
+{
+    uint num=users[_user_id_1].JointAcc[_user_id_2];
+    balance_ = JointAccList[num].user_id_1_bal+JointAccList[num].user_id_2_bal;
+}
 
 function sendAmount(uint _user_id_1, uint _user_id_2,uint amount)public{
     
@@ -66,6 +90,7 @@ function sendAmount(uint _user_id_1, uint _user_id_2,uint amount)public{
     uint vertex;
     bool visit_status;
     bool found=false;
+    bool status;
     
     while (first<=last){
         
@@ -87,12 +112,16 @@ function sendAmount(uint _user_id_1, uint _user_id_2,uint amount)public{
             break;
         }
         }
-        if (visit_status=false){
+        if (visit_status==false){
         for(uint i=0;i<adjmatrix[vertex].length;i++){
             new_path=path;
             new_path.push(adjmatrix[vertex][i]);
+            status=verifybalance(vertex,adjmatrix[vertex][i],amount);
+            if (status==true){
             queue.push(new_path);
             last+=1;
+            }
+            
             delete new_path;
         }
         visited.push(vertex);
@@ -119,5 +148,4 @@ function closeAccount(uint _user_id_1, uint _user_id_2) public{
 
 
 }
-
 
